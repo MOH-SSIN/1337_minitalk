76ad3245c666c6f44e23b3e487a855cd94c10235
Mohcine Ez Zahi <mez-zahi@e3r10p12.1337.ma>
2025-04-07 17:27:49 +0100

    minitalk

--- bonus/minitalk_bonus.h
+++ bonus/minitalk_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mez-zahi <mez-zahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:53:24 by mez-zahi          #+#    #+#             */
-/*   Updated: 2025/04/07 13:10:02 by mez-zahi         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:16:14 by mez-zahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,21 @@
 # include <stdbool.h>
 # include "../libft/libft.h"
 
+typedef struct s_data
+{
+    int bit;
+    int byte_index;
+    int expected_bytes;
+    int client_pid;
+} t_data;
+
 // client_utils
-void	check_argument(int argc, char **argv);
-void	envoyer_message(int pid, char *message);
+void    check_argument(int argc, char **argv);
+void    envoyer_message(int pid, char *message);
 
 // server_utils
-void	traiter_signal(int sig, siginfo_t *info, void *context);
-void	configurer_signaux(void);
-
+void    traiter_signal(int sig, siginfo_t *info, void *context);
+void    configurer_signaux(void);
+int     ft_which_byte(unsigned char buffer[4]);
+void    do_process(t_data *set, unsigned char *buffer);
 #endif
--- bonus/server_utlis_bonus.c
+++ bonus/server_utlis_bonus.c
@@ -6,57 +6,107 @@
 /*   By: mez-zahi <mez-zahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:49:00 by mez-zahi          #+#    #+#             */
-/*   Updated: 2025/04/07 13:09:34 by mez-zahi         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:17:20 by mez-zahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../bonus/minitalk_bonus.h"
 
-void	traiter_signal(int sig, siginfo_t *info, void *context)
-{
-	static int	bit_count = 0;
-	static char	received_char = 0;
-	static int	pid = 0;
 
-	(void)context;
-	if (pid != info->si_pid)
-	{
-		pid = info->si_pid;
-		bit_count = 0;
-		received_char = 0;
-	}
-	if (sig == SIGUSR2)
-		received_char |= (1 << (7 - bit_count));
-	bit_count++;
-	if (bit_count == 8) // Message complet reçu
+
+// Fonction pour déterminer le nombre d'octets attendus pour un caractère UTF-8
+int ft_which_byte(unsigned char buffer[4])
+{
+    int expected_bytes;
+
+    expected_bytes = 0;
+    if ((buffer[0] & 128) == 0)
+        expected_bytes = 1;
+    else if ((buffer[0] & 224) == 192)
+        expected_bytes = 2;
+    else if ((buffer[0] & 240) == 224)
+        expected_bytes = 3;
+    else if ((buffer[0] & 248) == 240)
+        expected_bytes = 4;
+    return (expected_bytes);
+}
+
+void do_process(t_data *set, unsigned char *buffer)
+{
+    if (set->bit == 8)
     {
-		//hadi li zate !! ack
-        if (received_char == '\0')  // Si c'est le caractère de fin de message
+        set->bit = 0;
+        if (set->byte_index == 0)
+            set->expected_bytes = ft_which_byte(buffer);
+        (set->byte_index)++;
+        
+        if (set->byte_index == set->expected_bytes)
         {
-            kill(pid, SIGUSR1);  // Envoie de l'ACK au client
-			// exit(1);
+            // Si c'est la fin du message (caractère nul)
+            if (buffer[0] == '\0')
+            {
+                write(1, "\n", 1);
+                kill(set->client_pid, SIGUSR1);  // Envoie de l'ACK au client
+            }
+            else
+                write(1, buffer, set->expected_bytes);
+            
+            // Réinitialiser pour le prochain caractère
+            set->byte_index = 0;
+            ft_memset(buffer, 0, 4);
         }
-        write(1, &received_char, 1); // Affiche le message reçu
-        bit_count = 0;
-        received_char = 0;
+        // Si on a reçu un caractère nul (fin de message)
+        else if (buffer[set->byte_index - 1] == '\0')
+        {
+            set->byte_index = 0;
+            ft_memset(buffer, 0, 4);
+        }
     }
 }
 
-void	configurer_signaux(void)
+void traiter_signal(int sig, siginfo_t *info, void *context)
 {
-	struct sigaction	sa;
-
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = traiter_signal;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-	{
-		ft_putstr_fd("Erreur : Impossible de configurer SIGUSR1.\n", 2);
-		exit(1);
-	}
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-	{
-		ft_putstr_fd("Erreur : Impossible de configurer SIGUSR2.\n", 2);
-		exit(1);
-	}
+    static unsigned char buffer[4] = {0};
+    static t_data set = {0, 0, 0, 0};
+    
+    (void)context;
+    
+    // Si c'est un nouveau client, réinitialiser les variables
+    if (set.client_pid != info->si_pid)
+    {
+        set.client_pid = info->si_pid;
+        set.bit = 0;
+        set.byte_index = 0;
+        set.expected_bytes = 0;
+        ft_memset(buffer, 0, 4);
+    }
+    
+    // Traiter le bit reçu
+    if (sig == SIGUSR2)
+        buffer[set.byte_index] = (buffer[set.byte_index] << 1) | 1;
+    else if (sig == SIGUSR1)
+        buffer[set.byte_index] = (buffer[set.byte_index] << 1);
+    
+    set.bit++;
+    do_process(&set, buffer);
 }
+
+void configurer_signaux(void)
+{
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = traiter_signal;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        ft_putstr_fd("Erreur : Impossible de configurer SIGUSR1.\n", 2);
+        exit(1);
+    }
+    if (sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        ft_putstr_fd("Erreur : Impossible de configurer SIGUSR2.\n", 2);
+        exit(1);
+    }
+}
+
