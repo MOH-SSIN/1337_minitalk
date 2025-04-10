ec42ab4b11daaad96e281bcec47ce1de4144e8a6
Mohcine Ez Zahi <mez-zahi@e3r9p18.1337.ma>
2025-04-10 17:14:13 +0100

    minitalk

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mez-zahi <mez-zahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:20:36 by mez-zahi          #+#    #+#             */
-/*   Updated: 2025/04/07 17:40:27 by mez-zahi         ###   ########.fr       */
+/*   Updated: 2025/04/10 16:42:30 by mez-zahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Usage : ./server (sans argument)\n", 2);
 		return (1);
 	}
-	configurer_signaux();
 	ft_putstr_fd("PID du serveur : ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putstr_fd("\n", 1);
+	configurer_signaux();
 	while (1)
 		pause();
 	return (0);
